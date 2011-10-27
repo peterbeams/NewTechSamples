dfdc11945c4e7bdb74177b378a9b70a9a6bbd009
Peter Beams <peter.beams@googlemail.com>
2011-10-27 16:26:18 +0100

    adding a sample to code contracts where pex has been used to discover edge cases

---
+++ CodeContractsSample/ContractsSample/ProductWithContractsPexxedUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace ContractsSample
+{
+    public class ProductWithContractsPexxedUp
+    {
+        public int Stock { get; private set; }
+        public string Name { get; set; }
+
+        public void AddStock(int value)
+        {
+            Contract.Requires(value > 0);
+            Contract.Requires((long)value + (long)Stock < int.MaxValue);
+            Stock += value;
+            Console.WriteLine(string.Concat("Adding ", value));
+        }
+
+        public void Pick(int value)
+        {
+            Contract.Requires(value <= Stock);
+            Contract.Requires(value > 0);
+            Stock -= value;
+            Console.WriteLine(string.Concat("Picking ", value));
+        }
+
+        [ContractInvariantMethod]
+        private void Invariant()
+        {
+            Contract.Invariant(Stock >= 0);
+        }
+    }
+}
